4f91a0d57e88049518b7259aee02221888b0005b
Arkadiy Vologodskiy <Vologodskiy20@gmail.com>
2022-08-17 23:56:05 +0500

    clock ez

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
+import Clock from './Clock/Clock';
 import './App.css';
+import months from './data';
 
 function App() {
+  const [hourDigital, setHourDigital] = useState("");
+  const [secondDigital, setSecondDigital] = useState("");
+  const [minutesDigital, setMinutesDigital] = useState("");
+  const [dayNow, setDayNow] = useState("");
+  const [monthNow, setMonthNow] = useState("");
+  const [yearNow, setYearNow] = useState("");
+  
+  useEffect(() => {
+    setInterval(() => {
+      let date = new Date();
+
+      let HH = date.getHours()
+      let MM = date.getMinutes()
+      let SS = date.getSeconds()
+      let day = date.getDate()
+      let month = date.getMonth()
+      let year = date.getFullYear()
+        
+      setSecondDigital(SS);
+      setHourDigital(HH);
+      setMinutesDigital(MM);
+      setDayNow(day);
+      setMonthNow(months[month]);
+      setYearNow(year);
+    }, 1000);
+  }, []);
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className='Clock'>
+        <Clock/>
+      </div>
+
+      <div className='digitalClock'>
+        
+      <div className='dataTime'>
+        <span className='data0'>День:{dayNow} </span>
+        <span className='data1'>Месец:{monthNow} </span>
+        <span className='data2'>Год:{yearNow} </span>
+      </div>
+      <div className='time'>
+        <span className='hour'>{hourDigital}</span>
+        <span className='min'>:{minutesDigital}</span>
+        <span className='seconds'>:{secondDigital}</span>
+        </div>
+        
+      
+      </div>
     </div>
   );
 }
